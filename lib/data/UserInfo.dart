2389d451923c120a8aea579373ed9ad17793f977
Ri-2020 <rohitgupta111abcd@gmail.com>
2022-08-01 11:27:30 +0530

    first commit

---
+++ lib/data/UserInfo.dart
@@ -0,0 +1,172 @@
+import 'package:whatsapp/models/chat_model.dart';
+
+List<ChatModel> chatModel = [
+  ChatModel(
+    name: "Ram Sharma",
+    message: "Hey, How do you do?",
+    time: "15: 20",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  ChatModel(
+    name: "thakur",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  ChatModel(
+    name: "singh",
+    message: "ha ha ha!!",
+    time: "17: 17",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: true,
+  ),
+];
+List<UserModel> userModel = [
+  UserModel(
+    name: "Paaso",
+    status: "U may call me VICTAR",
+    message: "Hey, How do you do?",
+    time: "15: 20",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Ruthu",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Vankatya",
+    message: "ha ha ha!!",
+    time: "17: 17",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: true,
+  ),
+  UserModel(
+    name: "Salmi",
+    status: "U may call me VICTAR",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "CST",
+    status: "At Work",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Dirym",
+    status: "Busy",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "splty",
+    status: "Like a sun",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "lons",
+    status: "Single hi hu",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "kdnf jdfn",
+    status: "A for apple, b for ball duniya is goal",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "sultan",
+    status: "..For everyone",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Monhan",
+    status: "Har Har Mahadev",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Krist",
+    status: "The most powerful words other than I LOVE YOU vujgug hvug ",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Nimit",
+    status: "Breaks your limits and outgrow yourself",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Putick",
+    status: "Konichiwa meena san",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "bisht",
+    status: "It's only and only about me",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+  UserModel(
+    name: "Grsad",
+    status: "Urgent call only",
+    message: "Are you ready!!!",
+    time: "17: 14",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-W_7SWMP5Rag/YTuyV5XvtUI/AAAAAAAAuUQ/hm6bYcvlFgQqgv1uosog6K8y0dC9eglTQCLcBGAsYHQ/w1200-h630-p-k-no-nu/Best-Profile-Pic-For-Boys%2B%25281%2529.jpg",
+    isgroup: false,
+  ),
+];
